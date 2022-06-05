be29af0fd607584f9df5504f3cc6b80ce7204ca1
Aishika Das <101255417+im-aishika@users.noreply.github.com>
2022-06-05 20:27:54 +0530

    Create Set_Matrix_Zero.cpp

---
+++ Day1/Set_Matrix_Zero.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+void setZeros(vector<vector<int>> &matrix)
+{
+	bool col0 = false;
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    for(int i = 0; i < n; i++) {
+        if(matrix[i][0] == 0) {
+            col0 = true;
+        }
+        
+        for(int j = 1; j < m; j++) {
+            if(matrix[i][j] == 0) {
+                matrix[i][0] = matrix[0][j] = 0;
+            }
+        }
+    }
+    
+    for(int i = n-1; i >= 0; i--) {
+        for(int j = m-1; j >= 1; j--) {
+            if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+            }
+        }
+        if(col0 == true) {
+            matrix[i][0] = 0;
+        }
+    }
+    
+}
