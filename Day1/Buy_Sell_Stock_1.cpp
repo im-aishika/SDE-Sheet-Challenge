1ae14a91881199ab4d90837f9f8cc58e2c57a2ef
Aishika Das <101255417+im-aishika@users.noreply.github.com>
2022-06-05 21:39:13 +0530

    Create Buy_Sell_Stock_1.cpp

---
+++ Day1/Buy_Sell_Stock_1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    int maxProfit = 0;
+    int minPrice = INT_MAX;
+    for(int i = 0; i < prices.size() ; i++) {
+        minPrice = min(minPrice, prices[i]);
+        maxProfit = max(maxProfit, prices[i] - minPrice);
+    }
+    return maxProfit;
+}
