c0ade414d5110d2d57984d72ab5c22a4fc9e4686
Aishika Das <101255417+im-aishika@users.noreply.github.com>
2022-06-20 00:56:27 +0530

    Create PalindromePartition.cpp

---
+++ Day9/PalindromePartition.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+bool isPalin(string &s, int start, int end) {
+    while(start <= end) {
+        if(s[start++] != s[end--]) return false;
+    }
+    return true;
+}
+void func(int ind, string &s, vector<string> &ds, vector<vector<string>> &ans) {
+    if(ind == s.size()) {
+        ans.push_back(ds);
+        return;
+    }
+    for(int i = ind; i < s.size(); i++) {
+        if(isPalin(s, ind, i)) {
+            ds.push_back(s.substr(ind, i-ind+1));
+            func(i+1, s, ds, ans);
+            ds.pop_back();
+        }
+    }
+}
+vector<vector<string>> partition(string &s) 
+{
+    vector<string> ds;
+    vector<vector<string>> ans;
+    func(0, s, ds, ans);
+    return ans;
+}
